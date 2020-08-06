e796750034cccaae19e1972716bdc8260c64fb82
Samar <33544311+god-s-perfect-idiot@users.noreply.github.com>
2020-08-06 15:24:51 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,11 +1,19 @@
 def retrieve_pf():
     try:
         with open("layouts/template1/index.html") as f:
-            pf_data = f.read()
+            pdf = []
+            pf_data = f.readline()
+            while(pf_data!=""):
+                pdf.append(pf_data)
+                pf_data = f.readline()
         with open("layouts/template1/projects.html") as f:
-            projects_data = f.read()
+            pd = []
+            projects_data = f.readline()
+            while(projects_data != ""):
+                pd.append(projects_data)
+                projects_data = f.readline()
     except:
         print("File Not Found!?")
-        pf_data = "FNF"
-        projects_data = "FNF"
-    return(pf_data, projects_data)
+        pdf = "FNF"
+        pd = "FNF"
+    return(pdf, pd)
--- service.py
+++ service.py
@@ -1,7 +1,16 @@
 import model
 
 def update_pf(Data, pc, tc):
+
     pf,projects = model.retrieve_pf()
-    pf = pf.split("/n")
-    projects = projects.split("/n")
-    print(pf,projects)
+    print(Data)
+
+    pf[4] = "   <title>"+Data[0]+"</title>"
+    pf[32] = "      <a class=\"navbar-brand js-scroll\" href=\"#page-top\">"+Data[0]+"</a>"
+    pf[68] = "          <h1 class=\"intro-title mb-4\">"+Data[0]+"</h1>"
+    pf[69] = "          <p class=\"intro-subtitle\"><span class=\"text-slider-items\">"+Data[1]+","+Data[2]+","+Data[3]+","+Data[4]+"</span><strong class=\"text-slider\"></strong></p>"
+    pf[91] = "                      <p><span class=\"title-s\">Name: </span> <span>"+Data[0]+"</span></p>"
+    pf[92] = "                          <p><span class=\"title-s\">Profile: </span> <span>"+Data[1]+"</span></p>"
+    pf[93] = "                          <p><span class=\"title-s\">Email: </span> <span>"+Data[5]+"</span></p>"
+    pf[94] = "                          <p><span class=\"title-s\">Phone: </span> <span>"+Data[6]+"</span></p>"
+    
